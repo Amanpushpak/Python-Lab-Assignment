a724347f3eb18f9204bf2a13bce5bcf49ffc1f6a
Amanpushpak <100395222+Amanpushpak@users.noreply.github.com>
2022-05-09 13:13:52 +0530

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,9 @@
+print("Tis script is intended to swap te value of two data variable using 3rd temporary variable")
+print()
+a=int(input("Enter first number"))
+b=int(input("Enter second number"))
+print("values before swapping :a=",a,"b=",b)
+c=a
+a=b
+b=c
+print("values after swapping :a=",a,"b=",b)
